36276576bf2a12f776c2ab24bd7f071c4566d3bb
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-14 00:23:10 +0900

    #4.4 Route States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,47 @@
-import { useParams } from "react-router";
+import styled from "styled-components";
+import { useParams, useLocation } from "react-router";
+import { useState } from "react";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+  display: block;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>(); // hook
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -69,7 +69,6 @@ function Coins() {
       setLoading(false);
     })();
   }, []);
-  console.log(coins);
   return (
     <Container>
       <Header>
@@ -81,7 +80,12 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
                 <Img
                   src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
