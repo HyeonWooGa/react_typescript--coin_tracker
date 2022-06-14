2ae15d37c644686cd6fa54ff09dea73adc9905c0
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-14 23:51:40 +0900

    #6 Data Types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -30,20 +30,77 @@ interface RouteState {
   name: string;
 }
 
+interface InforData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>(); // hook
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [price, setPrice] = useState({});
+  const [info, setInfo] = useState<InforData>();
+  const [price, setPrice] = useState<PriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
+      console.log(infoData);
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
+      console.log(priceData);
       setInfo(infoData);
       setPrice(priceData);
     })();
@@ -59,3 +116,5 @@ function Coin() {
 }
 
 export default Coin;
+
+// 브라우저 콘솔에서 temp1, temp2 로 각 API 넘겨준뒤 Object.keys(temp1).join(), Object.values(temp1).map(v => typeof v).join() 으로 정보 받아와서 인터페이스 작성해줌
