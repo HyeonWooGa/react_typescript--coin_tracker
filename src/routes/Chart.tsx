66f72a49264602fee59e88c7da36d6153067dae0
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-19 23:09:58 +0900

    #12 Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -5,8 +5,19 @@ interface ChartProps {
   coinId: string;
 }
 
+interface IHistoricalData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistoricalData>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   return <h1>Chart</h1>;
